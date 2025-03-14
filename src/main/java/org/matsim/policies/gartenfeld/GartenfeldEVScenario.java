8caf8a283f7a0081252382baa58d9fdab2af7721
schlenther <schlenther@vsp.tu-berlin.de>
2025-03-14 10:08:13 +0100

    set up GartenfeldEVScenario. now aborts because legs have undefined travel time values (while their routes have travel times defined)

--- src/main/java/org/matsim/policies/gartenfeld/GartenfeldEVScenario.java
+++ src/main/java/org/matsim/policies/gartenfeld/GartenfeldEVScenario.java
@@ -1,37 +1,50 @@
 package org.matsim.policies.gartenfeld;
 
+import com.google.common.util.concurrent.AtomicDouble;
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.population.Leg;
 import org.matsim.application.MATSimApplication;
 import org.matsim.contrib.ev.EvConfigGroup;
 import org.matsim.contrib.ev.EvModule;
-import org.matsim.contrib.ev.infrastructure.ChargingInfrastructureSpecification;
-import org.matsim.contrib.ev.infrastructure.ChargingInfrastructureUtils;
+import org.matsim.contrib.ev.fleet.ElectricFleetUtils;
+import org.matsim.contrib.ev.strategic.StrategicChargingConfigGroup;
 import org.matsim.contrib.ev.strategic.StrategicChargingUtils;
-import org.matsim.contrib.ev.strategic.infrastructure.PersonChargerProvider;
+import org.matsim.contrib.ev.strategic.scoring.ChargingPlanScoringParameters;
+import org.matsim.contrib.ev.withinday.WithinDayEvEngine;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.QSimConfigGroup;
+import org.matsim.core.config.groups.VspExperimentalConfigGroup;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.router.TripStructureUtils;
+import org.matsim.vehicles.Vehicle;
+import org.matsim.vehicles.VehicleType;
+import org.matsim.vehicles.VehicleUtils;
+import org.matsim.vehicles.Vehicles;
+
 
 public class GartenfeldEVScenario extends GartenfeldScenario {
+
+    //normally i would like to put the https URL here, but it isn't possible to have MATSim application load a config from a URL, atm.
+//    private static final String SVN_OUPTUT_DIRECTORY = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/gartenfeld/output/gartenfeld-v6.4-cutout-1pct/";
+    private static final String SVN_OUPTUT_DIRECTORY = "D:/public-svn/matsim/scenarios/countries/de/gartenfeld/output/gartenfeld-v6.4-cutout-1pct/";
 
     public static void main(String[] args) {
         MATSimApplication.runWithDefaults(GartenfeldEVScenario.class, args,
                 "--parking-garages", "NO_GARAGE",
-                "--config:network.inputChangeEventsFile", "../../input/gartenfeld/gartenfeld-v6.4.network-change-events.xml.gz",
-                "--config:facilities.inputFacilitiesFile", "../../input/gartenfeld/gartenfeld-v6.4.facilities.xml.gz",
-                "--config:controller.runId", "gartenfeld-v6.4.cutout-base-1pct",
-                "--config:network.inputNetworkFile", "../../input/gartenfeld/gartenfeld-v6.4.network-cutout.xml.gz",
-                "--config:plans.inputPlansFile", "../../input/gartenfeld/gartenfeld-v6.4.population-cutout-1pct.xml.gz",
-                "--config:controller.lastIteration", "500",
-                "--config:controller.outputDirectory", "output/gartenfeld-v6.4.cutout-base-1pct/"
+                "--config", SVN_OUPTUT_DIRECTORY + "gartenfeld-v6.4-cutout-1pct.output_config.xml",
+                "--config:network.inputChangeEventsFile", "gartenfeld-v6.4-cutout-1pct.output_change_events.xml.gz",
+                "--config:facilities.inputFacilitiesFile", "gartenfeld-v6.4-cutout-1pct.output_facilities.xml.gz",
+                "--config:controller.runId", "gartenfeld-v6.4.cutout-1pct-EV",
+                "--config:network.inputNetworkFile", "gartenfeld-v6.4-cutout-1pct.output_network.xml.gz",
+                "--config:plans.inputPlansFile", "gartenfeld-v6.4-cutout-1pct.output_plans.xml.gz",
+                "--config:vehicles.vehiclesFile", "gartenfeld-v6.4-cutout-1pct.output_vehicles.xml.gz",
+                "--config:controller.outputDirectory", "output/gartenfeld-v6.4-cutout-1pct-ev-postsim/" ,
+                "--config:controller.lastIteration", "1",
+                "--config:controller.overwriteFiles", "deleteDirectoryIfExists"
         );
-    }
-
-    @Override
-    protected void prepareScenario(Scenario scenario) {
-        super.prepareScenario(scenario);
-
-        //TODO: activate strategic ev charging for (specific) persons
     }
 
     @Override
@@ -41,12 +54,78 @@ public class GartenfeldEVScenario extends GartenfeldScenario {
         // Add the EV config group
         EvConfigGroup evConfigGroup = ConfigUtils.addOrGetModule(config, EvConfigGroup.class);
         // Set the charging infrastructure file
-        evConfigGroup.chargersFile = "../../input/gartenfeld/gartenfeld-v6.4.chargers.xml.gz";
+        evConfigGroup.chargersFile = "../../input/chargers/gartenfeld-v6.4.chargers.xml";
 
-        //deletes all 'normal' MATSim replanning strategies and configures only replanning for charging
+        // Deletes all 'normal' MATSim replanning strategies and configures replanning for charging, only.
+        // Also sets maxPlanMemorySize (for transport plans) to 1
         StrategicChargingUtils.configureStanadlone(config);
+        // the VSPConfigConsistencyChecker complains about us having no strategy that uses ChangeExpBeta.
+        // However, here we do not want _any_ transport replanning, but only replanning of EV charging (which is why we call StrategicChargingUtils.configureStanadlone(config); above).
+        // So we stop the checker from aborting
+        config.vspExperimental().setVspDefaultsCheckingLevel(VspExperimentalConfigGroup.VspDefaultsCheckingLevel.warn);
+
+
+        StrategicChargingConfigGroup strategicChargingConfigGroup = ConfigUtils.addOrGetModule(config, StrategicChargingConfigGroup.class);
+        //TODO configure the StrategicChargingConfigGroup
+
+        //configure the scoring parameters for the strategic charging
+        ChargingPlanScoringParameters chargingScoringParameters = new ChargingPlanScoringParameters();
+        //TODO configure the scoring parameters
+        strategicChargingConfigGroup.scoring = chargingScoringParameters;
+
+        // Because we take simulation output as input, all vehicles are actually created/contained in the vehicles file.
+        // Later (in prepareScenario), we will just override the vehicle type for a few vehicles
+        // This means we do not want to (re-) create the vehicles from the vehicle types, and we do want different vehicle types for vehicles of the same mode.
+        // Thus we change the vehicles source away from QSimConfigGroup.VehiclesSource.modeVehicleTypesFromVehiclesData
+        config.qsim().setVehiclesSource(QSimConfigGroup.VehiclesSource.fromVehiclesData);
 
         return config;
+    }
+
+    @Override
+    protected void prepareScenario(Scenario scenario) {
+        super.prepareScenario(scenario);
+
+        Vehicles vehicles = scenario.getVehicles();
+        //create the vehicle type for EVs
+        VehicleType evType = VehicleUtils.createVehicleType(Id.create("electric_car", VehicleType.class), TransportMode.car);
+        //copy all attributes from the car type
+        VehicleUtils.copyFromTo(vehicles.getVehicleTypes().get(Id.create("car", VehicleType.class)), evType);
+        //set ev attributes
+        VehicleUtils.setHbefaTechnology(evType.getEngineInformation(), ElectricFleetUtils.EV_ENGINE_HBEFA_TECHNOLOGY);
+        VehicleUtils.setEnergyCapacity(evType.getEngineInformation(), 100);
+
+//        ElectricFleetUtils.setChargerTypes(carVehicleType, Arrays.asList("a", "b", "default" ) );
+
+        //add the ev type
+        scenario.getVehicles().addVehicleType(evType);
+
+        //activate strategic ev charging for all Gartenfeld inhabitants and change their cars to be evs
+        scenario.getPopulation().getPersons().values().stream()
+                .filter(person -> person.getId().toString().contains("dng"))
+                .forEach(person -> {
+                    WithinDayEvEngine.activate(person);
+                    Id<Vehicle> vehicleId = VehicleUtils.getVehicleId(person, TransportMode.car);
+
+                    //we can not override the vehicle type, so we have to recreate the vehicle.
+                    vehicles.removeVehicle(vehicleId);
+                    Vehicle newVehicle = VehicleUtils.createVehicle(vehicleId, evType);
+                    ElectricFleetUtils.setInitialSoc(newVehicle, 0.7); //TODO think about a better way to set the initial SoC
+                    vehicles.addVehicle(newVehicle);
+
+
+                    //debugging
+                        AtomicDouble travelTime = new AtomicDouble(0.0);
+                        AtomicDouble travelDistance = new AtomicDouble(0.0);
+
+                        for (Leg leg : TripStructureUtils.getLegs(person.getSelectedPlan())) {
+                            if (leg.getTravelTime().isUndefined()){
+                                System.out.println("undefined travel time for agent " + person.getId() + " at depTime" + leg.getDepartureTime() + " with mode " + leg.getMode());
+                            }
+                            travelTime.addAndGet(-leg.getTravelTime().seconds());
+                            travelDistance.addAndGet(-leg.getRoute().getDistance());
+                        }
+                });
     }
 
     @Override
